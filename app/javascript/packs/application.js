a7fc7ace15ad19584ea2dea965f4d7b4bb04645b
Lisa <llvvss@icloud.com>
2021-07-17 15:51:49 -0600

    more files and adds
    from Tailwind and Alpine installation with Rails, from https://btihen.me/post_ruby_rails/rails_6_1_tailwind_2_0_alpinejs/

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,12 +2,24 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
+// LS - text below added from https://btihen.me/post_ruby_rails/rails_6_1_tailwind_2_0_alpinejs/
 
 import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
+import "@hotwired/turbo-rails"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
+// import alpinejs and its necessary rails adaptation
+import 'alpine-turbo-drive-adapter'
+require("alpinejs")
+
+// import tailwind into javascript
+import "../stylesheets/application.scss"
+
 Rails.start()
-Turbolinks.start()
 ActiveStorage.start()
+
+import "controllers"
+
+require("trix")
+require("@rails/actiontext")
